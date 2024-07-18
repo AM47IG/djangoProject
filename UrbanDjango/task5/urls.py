f9770228e560df291194c3f3ff86ce228f2da6bd
AM47IG <157103936+AM47IG@users.noreply.github.com>
2024-07-18 18:26:46 +0800

    formy-otpravki-dannyh-html-i-django-formy

---
+++ UrbanDjango/task5/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import sign_up_by_django, sign_up_by_html
+
+
+urlpatterns = [
+    path('', sign_up_by_html),
+    path('django_sign_up', sign_up_by_django)
+]
---
+++ UrbanDjango/task5/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import UserRegister
+
+
+# Create your views here.
+def sign_up_by_html(request):
+    users = ['Иван', 'Мария']
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username') not in users
+        password = request.POST.get('password') == request.POST.get('repeat_password')
+        age = int(request.POST.get('age')) >= 18
+        if username and password and age:
+            return HttpResponse(f'Приветствуем, {request.POST.get('username')}!')
+        if not age:
+            info['error'] = 'Вы должны быть старше 18'
+        if not password:
+            info['error'] = 'Пароли не совпадают'
+        if not username:
+            info['error'] = 'Пользователь уже существует'
+    return render(request, 'fifth_task/registration_page.html', context=info)
+
+
+def sign_up_by_django(request):
+    users = ['Иван', 'Мария']
+    form = UserRegister()
+    info = {'form': form}
+    if request.method == 'POST':
+        form = UserRegister(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username') not in users
+            password = form.cleaned_data.get('password') == form.cleaned_data.get('repeat_password')
+            age = form.cleaned_data.get('age') >= 18
+            if username and password and age:
+                return HttpResponse(f'Приветствуем, {form.cleaned_data.get('username')}!')
+            if not age:
+                info['error'] = 'Вы должны быть старше 18'
+            if not password:
+                info['error'] = 'Пароли не совпадают'
+            if not username:
+                info['error'] = 'Пользователь уже существует'
+    return render(request, 'fifth_task/registration_page.html', context=info)
